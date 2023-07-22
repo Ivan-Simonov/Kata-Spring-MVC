554e0775e7c9c2724c5891c77f44e01a7bdd614a
Ivan Simonov <ivan.simonov.dev@gmail.com>
2023-07-22 20:13:23 +0300

    removed comments added equals/hashcode for model

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,5 +1,7 @@
 package web.model;
 
+import java.util.Objects;
+
 public class Car {
     private int id;
     private String manufacturer;
@@ -33,5 +35,18 @@ public class Car {
 
     public void setPrice(String price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
